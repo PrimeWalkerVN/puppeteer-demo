76852842e0cde12227d3a4c7935f7cce94ecd365
PrimeWalkerVN <primewalkervn@gmail.com>
2022-09-13 14:26:13 +0700

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # puppeteer-demo
+
+Crawl data using puppeteer on http://books.toscrape.com/ with category and save into the file
---
+++ pageController.js
@@ -0,0 +1,33 @@
+const pageScraper = require("./pageScraper");
+const fs = require("fs");
+async function scrapeAll(browserInstance) {
+  let browser;
+  try {
+    browser = await browserInstance;
+    let scrapedData = {};
+    // Call the scraper for different set of books to be scraped
+    scrapedData["Travel"] = await pageScraper.scraper(browser, "Travel");
+    scrapedData["Romance"] = await pageScraper.scraper(browser, "Romance");
+    scrapedData["Fantasy"] = await pageScraper.scraper(browser, "Fantasy");
+    await browser.close();
+
+    fs.writeFile(
+      "data.json",
+      JSON.stringify(scrapedData),
+      "utf8",
+      function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log(
+          "The data has been scraped and saved successfully! View it at './data.json'"
+        );
+      }
+    );
+    console.log(scrapedData);
+  } catch (err) {
+    console.log("Could not resolve the browser instance => ", err);
+  }
+}
+
+module.exports = (browserInstance) => scrapeAll(browserInstance);
